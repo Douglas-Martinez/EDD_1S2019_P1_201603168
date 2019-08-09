03a178ff71b8063d120637f1875beaca82316cc1
Douglas-Martinez <53403994+Douglas-Martinez@users.noreply.github.com>
2019-08-09 17:06:15 -0600

    Solo creacion de las clases de las EDD's

---
+++ Estructuras.py
@@ -0,0 +1,13 @@
+from Nodos import NodoSerpiente, NodoPila, NodoCola, NodoUsuario
+
+class ListaDoble(object):
+    def __init__(self):
+
+class Pila(object):
+    def __init__(self):
+
+class Cola(object):
+    def __init__(self):
+
+class ListaCircularDoble(object):
+    def __init__(self):
